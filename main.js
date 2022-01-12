a4d8e9ea82bfb30b789f6e1a756a5ef44cb84658
jc9361 <jc9361@gmail.com>
2022-01-11 21:56:54 -0800

    adding smooth-scrolling, scroll_to_top, dark-mode, and link_to_github js buttons.

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-// alert("BETA 1/1/22");
+
 console.log("hello world");
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+// SETTINGS
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+});
 
+
+// FUNCTIONS
+function link_to_github() {
+    window.open(
+        "https://github.com/jc9361"
+    )
+}
+
+
+function scroll_to_top() {
+    window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+
+function dark_mode_toggle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
