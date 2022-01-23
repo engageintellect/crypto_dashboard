57e4e663d4e8efa8a0edfaf8a6af35fd0860a625
jc9361 <jc9361@gmail.com>
2022-01-22 23:34:09 -0800

    V_0.0.2

---
+++ dashboard/main.js
@@ -0,0 +1,31 @@
+
+console.log("hello world");
+
+// SETTINGS
+window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth'
+});
+
+
+// FUNCTIONS
+function link_to_github() {
+    window.open(
+        "https://github.com/jc9361"
+    )
+}
+
+
+function scroll_to_top() {
+    window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+
+function dark_mode_toggle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
